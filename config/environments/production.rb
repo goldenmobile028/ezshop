ba1c697360db0bf91f4809aff6e7c85c312ae9d3
huawan028 <huawan028@163.com>
2017-10-20 09:38:54 +0800

    Change sendgrid username and password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,8 @@ Rails.application.configure do
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => 'Delucien',
-  :password       => 'Meroline22$',
+  :user_name      => 'Delucien1',
+  :password       => 'Meroline21$',
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
