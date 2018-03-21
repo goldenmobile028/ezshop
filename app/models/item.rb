fb0d90b9f940018961734d793ca1a9b88f0813f9
huawan028 <huawan028@163.com>
2018-03-22 05:10:51 +0800

    commit for item beacon_id update

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,7 @@ has_many :item_favorites
      latitudeDelta: self.latitudeDelta, longitudeDelta: self.longitudeDelta,
      favorite: item.present? ? item.favorite : false,
      location: self.location ? self.location : "",
-     beacon_id: self.beacon_id ? self.beacon_id : ""}
+     beacon_id: self.beacon_id.nil? ? "" : self.beacon_id}
   end
 
 end
